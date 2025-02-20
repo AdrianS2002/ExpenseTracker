ffb5c3f74bb0c43b99955c8d02ac208286d98d15
Raluca <spatarularisaraluca@gmail.com>
2025-02-20 19:27:29 +0200

    Add html and css for DayPicker component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DayPickerComponent } from "./expenses-tracker/day-picker/day-picker.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DayPickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
