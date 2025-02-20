3e560ff5717fd6e298bb45b2e6a48695aa2f81e2
Diana <dianamariapetrula@gmail.com>
2025-02-21 01:52:38 +0200

    Added not-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth', pathMatch: 'full' },
-    {path: 'auth', component: AuthComponent},
+  { path: 'auth', component: AuthComponent },
+  { path: '**', component: NotFoundComponent }
 ];
