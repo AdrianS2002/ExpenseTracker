5f94ad1047e7d042b4a42a4187565cc5e265dc23
Raluca <spatarularisaraluca@gmail.com>
2025-02-20 19:41:58 +0200

    Add html and css for expense component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DayPickerComponent } from "./expenses-tracker/day-picker/day-picker.component";
+import { ExpensesListComponent } from "./expenses-tracker/expenses-list/expenses-list.component";
+import { ExpenseComponent } from "./expenses-tracker/expenses-list/expense/expense.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DayPickerComponent],
+  imports: [RouterOutlet, DayPickerComponent, ExpensesListComponent, ExpenseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
