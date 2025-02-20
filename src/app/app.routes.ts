5d610407252c55da00d8dc9dc18a70035786b4b8
Lucky7 <mihaitibrea2@gmail.com>
2025-02-20 13:20:16 +0200

    adaugat ruta summary

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SummaryComponent } from './summary/summary.component';
 
-export const routes: Routes = []
+const routes: Routes = [
+  { path: 'summary', component: SummaryComponent },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
