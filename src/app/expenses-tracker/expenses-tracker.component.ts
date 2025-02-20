05e5052191668974992d71888b4ffa830bd91619
Raluca <spatarularisaraluca@gmail.com>
2025-02-20 19:11:21 +0200

    Add initial components for expenses traker

---
+++ src/app/expenses-tracker/expenses-tracker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expenses-tracker',
+  standalone: true,
+  imports: [],
+  templateUrl: './expenses-tracker.component.html',
+  styleUrl: './expenses-tracker.component.css'
+})
+export class ExpensesTrackerComponent {
+
+}
