ed061474485479a4ffac4400a4faacfa7a89a259
Raluca <spatarularisaraluca@gmail.com>
2025-02-22 19:09:28 +0200

    Update routing to navigate to ExpensesTrackerComponent after login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { ExpensesTrackerComponent } from './expenses-tracker/expenses-tracker.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth', pathMatch: 'full' },
     {path: 'auth', component: AuthComponent},
+    {path: 'expense-tracker', component: ExpensesTrackerComponent}
 ];
