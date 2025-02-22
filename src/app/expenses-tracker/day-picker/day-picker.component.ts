15fc0b4ec92c1481a7c2c1e3f8d3d0bfb599ed32
Raluca <spatarularisaraluca@gmail.com>
2025-02-22 18:18:14 +0200

    Implement day selection feature in DayPicker component with styling updates

--- src/app/expenses-tracker/day-picker/day-picker.component.ts
+++ src/app/expenses-tracker/day-picker/day-picker.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ExpensesTrackerService } from '../expenses-tracker.service';
 
 @Component({
   selector: 'app-day-picker',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './day-picker.component.css'
 })
 export class DayPickerComponent {
-
+  daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  expensesTrackerService = inject(ExpensesTrackerService);
 }
--- src/app/expenses-tracker/expenses-tracker.service.ts
+++ src/app/expenses-tracker/expenses-tracker.service.ts
@@ -4,5 +4,15 @@ import { Injectable, Signal, signal } from '@angular/core';
   providedIn: 'root'
 })
 export class ExpensesTrackerService {
+  private selectedDay = signal("Monday");
   constructor() { }
+
+  getSelectedDay(): Signal<string> {
+    return this.selectedDay;
+  }
+
+  setSelectedDay(day: string): void {
+    console.log("Setting day to: ", day);
+    this.selectedDay.set(day);
+  }
 }
