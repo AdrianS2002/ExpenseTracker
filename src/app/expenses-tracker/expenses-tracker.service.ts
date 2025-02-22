8adfb8c42852bdf8abc69bc4b23a7da9b82996b7
Raluca <spatarularisaraluca@gmail.com>
2025-02-22 18:41:15 +0200

    Refactor DayPicker component to implement day availability logic and update styling for available and unavailable days

--- src/app/expenses-tracker/expenses-tracker.service.ts
+++ src/app/expenses-tracker/expenses-tracker.service.ts
@@ -5,14 +5,51 @@ import { Injectable, Signal, signal } from '@angular/core';
 })
 export class ExpensesTrackerService {
   private selectedDay = signal("Monday");
-  constructor() { }
+  private availableDays = [''];
+  private unavailableDays = [''];
+
+  constructor() { 
+    this.determineAvailableDays();
+  }
 
   getSelectedDay(): Signal<string> {
     return this.selectedDay;
   }
 
   setSelectedDay(day: string): void {
+    if(!this.availableDays.includes(day)) {
+      return;
+    }
+
     console.log("Setting day to: ", day);
     this.selectedDay.set(day);
   }
+
+  getAvailableDays(): string[] {
+    return this.availableDays;
+  }
+
+  getUnavailableDays(): string[] {
+    return this.unavailableDays;
+  }
+
+  determineAvailableDays(): void {
+    const currentDay = new Date().getDay();
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    this.availableDays = daysOfWeek.slice(0, currentDay);
+    this.unavailableDays = daysOfWeek.slice(currentDay);
+  }
+
+  determineDayClass(day: string): string {
+    if(this.selectedDay() === day) {
+      return "selectedDay";
+    }
+
+    if(this.availableDays.includes(day)) {
+      return "availableDay";
+    }
+
+    return "unavailableDay";
+} 
 }
