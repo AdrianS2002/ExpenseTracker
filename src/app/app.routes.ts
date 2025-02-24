c38cf8ec43c048f541a1170ddff0b3664f724fc6
Lucky7 <mihaitibrea2@gmail.com>
2025-02-24 13:37:55 +0200

    reparat sidebar sa nu mai ocupe tot ecranul

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { SummaryComponent } from './summary/summary.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'auth', pathMatch: 'full' },
-    { path: 'forgot-password', component: ForgotPasswordComponent},
-    {path: 'auth', component: AuthComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'summary', component: SummaryComponent },
 ];
