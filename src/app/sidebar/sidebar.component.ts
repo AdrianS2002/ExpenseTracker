2f7e2854a30b5c4bceac1e0eb0a4ae03b7249b97
Lucky7 <mihaitibrea2@gmail.com>
2025-02-24 14:03:48 +0200

    adaugat hamburger

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  isActive = false;
 
+  toggleSidebar(): void {
+    this.isActive = !this.isActive;
+  }
 }
