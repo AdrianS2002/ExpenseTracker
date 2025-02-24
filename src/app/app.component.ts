4451074f918a16892219d886005c6cfd626e14dd
Lucky7 <mihaitibrea2@gmail.com>
2025-02-24 13:22:54 +0200

    adaugat toate componentele

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { ExpensesTrackerComponent } from "./expenses-tracker/expenses-tracker.component";
+import { ExpensesTrackerComponent } from './expenses-tracker/expenses-tracker.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ExpensesTrackerComponent],
+  imports: [
+    RouterOutlet, 
+    ExpensesTrackerComponent, 
+    HeaderComponent, 
+    SidebarComponent, 
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ExpenseTracker';
